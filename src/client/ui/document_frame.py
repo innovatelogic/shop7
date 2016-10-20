8919680e062bbd5c2a1fedd42eefadd7ef70bbb9
YurG <yura.gunko@gmail.com>
2016-10-20 20:25:19 +0300

    upd

--- src/client/ui/document_frame.py
+++ src/client/ui/document_frame.py
@@ -1,12 +1,19 @@
 import wx
 from proportional_splitter import ProportionalSplitter
-from groups_tree_view import GroupsTreeView
 from status_view_panel import StatusViewPanel
 from doc_control_panel import DocControlPanel
-
+from document_view_panel import DocumentViewPanel
 TITLE_DLG = "Client"
 ONLINE_PANEL_HEIGHT = 50
 LEFT_PANEL_WIDTH = 100
+
+
+class EPanels:
+    EPanel_Cases = 0
+    EPanel_Clients = 1
+    EPanel_Connect = 2
+    EPanel_Settings = 3
+    EPanel_MAX = 4
 
 class DocumentFrame(wx.Frame):
     logout_flag = False
@@ -14,9 +21,8 @@ class DocumentFrame(wx.Frame):
         wx.Frame.__init__(self, parent, title=TITLE_DLG, size=(800, 600))
         self.connection_info = connection_info
         self.ms_connection = ms_connection
-        
         DocumentFrame.logout_flag = False
-        
+        self.view_panels = []
         self.InitInterface()
                 
         self.Show(True)
@@ -29,7 +35,7 @@ class DocumentFrame(wx.Frame):
         menuBar.Append(menu, "&File")
         menu = wx.Menu()
         m_about = menu.Append(wx.ID_ABOUT, "&About", "Information about this program")
-        #self.Bind(wx.EVT_MENU, self.OnAbout, m_about)
+        
         menuBar.Append(menu, "&Help")
         self.SetMenuBar(menuBar)
 
@@ -37,7 +43,7 @@ class DocumentFrame(wx.Frame):
         
         self.doLayout();
         
-        self.Bind(wx.EVT_SIZE, self.OnReSize)
+        self.BindEvents()
         
     def doLayout(self):
         self.toppanel = StatusViewPanel(self.connection_info, self, wx.ID_ANY, size = (self.GetSize().GetWidth(), ONLINE_PANEL_HEIGHT), pos = (0, 0))
@@ -52,39 +58,39 @@ class DocumentFrame(wx.Frame):
         self.SetSizer(posVertSzr)
               
         left_panel_height = self.GetSize().GetHeight() - ONLINE_PANEL_HEIGHT
-        self.leftpanel = DocControlPanel(self.connection_info,self.bottompanel, wx.ID_ANY, size = (LEFT_PANEL_WIDTH, left_panel_height), pos = (0, ONLINE_PANEL_HEIGHT))
+        self.leftpanel = DocControlPanel(self.connection_info, 
+                                         self.bottompanel,
+                                         wx.ID_ANY,
+                                         size = (LEFT_PANEL_WIDTH, left_panel_height), pos = (0, ONLINE_PANEL_HEIGHT))
         
         center_panel_width = self.GetSize().GetWidth() - LEFT_PANEL_WIDTH
-        self.centerpanel = wx.Panel(self.bottompanel, wx.ID_ANY, size = (center_panel_width, left_panel_height), pos = (LEFT_PANEL_WIDTH, ONLINE_PANEL_HEIGHT))
-        self.centerpanel.SetBackgroundColour((255, 255, 255))
+        self.centerpanel = wx.Panel(self.bottompanel, wx.ID_ANY, size = (center_panel_width, left_panel_height),
+                                             pos = (LEFT_PANEL_WIDTH, ONLINE_PANEL_HEIGHT))
         
         posHorSzr = wx.BoxSizer(wx.HORIZONTAL)
         posHorSzr.Add(self.leftpanel, 0, wx.EXPAND)
         posHorSzr.Add(self.centerpanel, 1, wx.GROW)
         self.bottompanel.SetSizer(posHorSzr)
         
-        ###
-        self.centerpanel.toppanel = wx.Panel(self.centerpanel, wx.ID_ANY, size = (center_panel_width, 45), pos = (0, 0))
-        self.centerpanel.toppanel.SetBackgroundColour((215, 215, 215))
-        self.centerpanel.bottompanel = wx.Panel(self.centerpanel, wx.ID_ANY)
-        self.centerpanel.bottompanel.SetBackgroundColour((255, 255, 255))
+        # toggle panels
+        self.cases_panel = DocumentViewPanel(self.connection_info, self.ms_connection, 
+                                             self.centerpanel, wx.ID_ANY, size = (center_panel_width, left_panel_height))
+        self.clients_panel = wx.Panel(self.centerpanel, wx.ID_ANY, size = (center_panel_width, left_panel_height))
+        self.clients_panel.SetBackgroundColour((255, 0, 0))
+        self.connect_panel = wx.Panel(self.centerpanel, wx.ID_ANY, size = (center_panel_width, left_panel_height))
+        self.connect_panel.SetBackgroundColour((0, 255, 0))
+        self.settings_panel = wx.Panel(self.centerpanel, wx.ID_ANY, size = (center_panel_width, left_panel_height))
+        self.settings_panel.SetBackgroundColour((0, 255, 255))
         
-        posCenterPanelVertSzr = wx.BoxSizer(wx.VERTICAL)
-        posCenterPanelVertSzr.Add(self.centerpanel.toppanel, 0, wx.EXPAND)
-        posCenterPanelVertSzr.Add(self.centerpanel.bottompanel, 1, wx.GROW)
-        self.centerpanel.SetSizer(posCenterPanelVertSzr)
-                
-        self.centerpanel.bottompanel.left_tree = GroupsTreeView(self.ms_connection, self.centerpanel.bottompanel, 1, wx.DefaultPosition, (250, -1),
-                                                                 wx.TR_HIDE_ROOT|wx.TR_HAS_BUTTONS|wx.TR_LINES_AT_ROOT)
-        self.centerpanel.bottompanel.right = wx.Panel(self.centerpanel.bottompanel, wx.ID_ANY)
-        self.centerpanel.bottompanel.right.SetBackgroundColour((235, 235, 235))
+        self.view_panels.append(self.cases_panel)
+        self.view_panels.append(self.clients_panel)
+        self.view_panels.append(self.connect_panel)
+        self.view_panels.append(self.settings_panel)
         
-        posDocHorSzr = wx.BoxSizer(wx.HORIZONTAL)
-        posDocHorSzr.Add(self.centerpanel.bottompanel.left_tree, 0, wx.EXPAND)
-        posDocHorSzr.Add(self.centerpanel.bottompanel.right, 1, wx.GROW)
-        self.centerpanel.bottompanel.SetSizer(posDocHorSzr)
+        self.TogglePanel(EPanels.EPanel_Cases)
         
         self.Layout()
+        #self.cases_panel.Layout()
         
     def OnReSize(self, event):
             "Window has been resized, so we need to adjust the window."
@@ -96,3 +102,32 @@ class DocumentFrame(wx.Frame):
         
     def GetGroups(self, id):
         return self.ms_connection.send({'opcode': 'get_groups', 'id':id, 'token':self.connection_info['token']})
+    
+    def BindEvents(self):
+        self.Bind(wx.EVT_SIZE, self.OnReSize)
+        self.Bind(wx.EVT_BUTTON, self.OnClick_Cases, self.leftpanel.btn_cases)
+        self.Bind(wx.EVT_BUTTON, self.OnClick_Clients, self.leftpanel.btn_clients)
+        self.Bind(wx.EVT_BUTTON, self.OnClick_Connect, self.leftpanel.btn_conect)
+        self.Bind(wx.EVT_BUTTON, self.OnClick_Settings, self.leftpanel.btn_settings)
+        pass
+    
+    def TogglePanel(self, index):
+        for i in range(0, EPanels.EPanel_MAX):
+            if i == index:
+                self.view_panels[i].Show()
+                self.view_panels[i].Layout()
+            else:
+                self.view_panels[i].Hide()
+        self.centerpanel.Layout()
+        
+    def OnClick_Cases(self, event):
+        self.TogglePanel(EPanels.EPanel_Cases)
+        
+    def OnClick_Clients(self, event):
+        self.TogglePanel(EPanels.EPanel_Clients)
+    
+    def OnClick_Connect(self, event):
+        self.TogglePanel(EPanels.EPanel_Connect)
+    
+    def OnClick_Settings(self, event):
+        self.TogglePanel(EPanels.EPanel_Settings)
--- src/client/ui/document_view_panel.py
+++ src/client/ui/document_view_panel.py
@@ -1,5 +1,32 @@
 import wx
+from groups_tree_view import GroupsTreeView
                 
 class DocumentViewPanel(wx.Panel):
- def __init__(self, connection_info, parent, *args, **kwargs):
+    def __init__(self, connection_info, ms_connection, parent, *args, **kwargs):
         wx.Panel.__init__(self, parent, *args, **kwargs)
+        self.connection_info = connection_info
+        self.ms_connection = ms_connection
+        self.SetBackgroundColour((255, 255, 255))
+        self.doLayout()
+        
+    def doLayout(self):
+        W,H = self.GetSize()
+        self.toppanel = wx.Panel(self, wx.ID_ANY, size = (-1, 45), pos = (0, 0))
+        self.toppanel.SetBackgroundColour((215, 0, 215))
+        self.bottompanel = wx.Panel(self, wx.ID_ANY)
+        self.bottompanel.SetBackgroundColour((255, 255, 255))
+        
+        posCenterPanelVertSzr = wx.BoxSizer(wx.VERTICAL)
+        posCenterPanelVertSzr.Add(self.toppanel, 0, wx.EXPAND)
+        posCenterPanelVertSzr.Add(self.bottompanel, 1, wx.GROW)
+        self.SetSizer(posCenterPanelVertSzr)
+                
+        self.bottompanel.left_tree = GroupsTreeView(self.ms_connection, self.bottompanel, 1, wx.DefaultPosition, (250, -1),
+                                                                 wx.TR_HIDE_ROOT|wx.TR_HAS_BUTTONS|wx.TR_LINES_AT_ROOT)
+        self.bottompanel.right = wx.Panel(self.bottompanel, wx.ID_ANY)
+        self.bottompanel.right.SetBackgroundColour((235, 235, 235))
+        
+        posDocHorSzr = wx.BoxSizer(wx.HORIZONTAL)
+        posDocHorSzr.Add(self.bottompanel.left_tree, 0, wx.EXPAND)
+        posDocHorSzr.Add(self.bottompanel.right, 1, wx.GROW)
+        self.bottompanel.SetSizer(posDocHorSzr)
